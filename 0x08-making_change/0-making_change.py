dab58618f67ff72157a827d07598887aa3a038ee
ea-boahen <ea.boahen2015@gmail.com>
2023-11-24 05:02:47 +0000

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unique Change making module.
+"""
+
+
+def makeChange_unique(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    remaining_amount = total
+    coins_count = 0
+    coin_index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    num_coins = len(coins)
+    while remaining_amount > 0:
+        if coin_index >= num_coins:
+            return -1
+        if remaining_amount - sorted_coins[coin_index] >= 0:
+            remaining_amount -= sorted_coins[coin_index]
+            coins_count += 1
+        else:
+            coin_index += 1
+    return coins_count
+
